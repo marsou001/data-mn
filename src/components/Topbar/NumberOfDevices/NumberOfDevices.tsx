4b7e26e5799edb5912f621954bae605e237c8cdf
Marouane Souda <marsou431@gmail.com>
2021-06-29 22:02:11 +0100

    feat: add functionality to number devices

--- src/components/Topbar/NumberOfDevices/NumberOfDevices.tsx
+++ src/components/Topbar/NumberOfDevices/NumberOfDevices.tsx
@@ -1,10 +1,17 @@
+import { useContext } from 'react';
 import { Container, Option, Title, Content } from '../styles';
+import { Context } from '../../../App';
+import { IData } from '../../../interfaces/dates';
 
 function NumberOfDevices() {
+    const { devices } = useContext(Context);
+
+    const numberOfDevices = Array.from(new Set([...devices.map((device: IData) => device.DeviceID)])).length;
+
     return (
         <Container>
             <Option>
-                <Title>Number devices</Title> <Content>22</Content>
+                <Title>Number devices</Title> <Content>{numberOfDevices}</Content>
             </Option>
         </Container>
     );
--- src/interfaces/devices.ts
+++ src/interfaces/devices.ts
@@ -1,1 +1,42 @@
-export let i = 'i';
+  
+export interface Devices {
+    _id: number;
+    deviceID: keyof Data;
+}
+
+export interface Data {
+    MAC000002: Device;
+    MAC000003: Device;
+    MAC000004: Device;
+}
+
+export interface Device {
+    _id: number;
+    deviceID: string;
+    constantData: ConstantData;
+    lastMonthData: LastMonthData[];
+    monthlyData: MonthlyData[];
+}
+
+interface ConstantData {
+    clientNumber: number,
+    longitude: number,
+    latitude: number,
+    address: string,
+    tarification: string,
+}
+
+interface LastMonthData {
+    id: number;
+    title: string;
+    content: number;
+}
+
+interface MonthlyData {
+    id: number,
+    date: string,
+    longitude: number,
+    latitude: number,
+    consumption: number,
+    dynamicPrice: number,
+}
