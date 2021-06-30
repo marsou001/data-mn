ee80d56d0611a890382d0f5ac6c20eea13d06420
Marouane Souda <marsou431@gmail.com>
2021-06-30 16:29:12 +0100

    feat: add layout to PDF invoice

--- src/components/Main/Billing/Billing.tsx
+++ src/components/Main/Billing/Billing.tsx
@@ -84,6 +84,43 @@ const styles = StyleSheet.create({
         textAlign: 'right',
         width: 80,
     },
+    invoiceTitle: {
+        color: '#666',        
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 40,
+    },
+    right: {
+        textAlign: 'right',
+    },
+    small: {
+        width: 80,
+    },
+    medium: {
+        width: 120,
+    },
+    large: {
+        width: 180,
+    },
+    flex: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    blackColor: {
+        color: '#000',
+    },
+    greyColor: {
+        color: '#666',
+    },
+    padding: {
+        padding: 20,
+    },
+    lightGreyBackground: {
+        backgroundColor: '#eee',
+    },
+    borderBottom: {
+        borderBottom: '1px solid #ccc',
+    },
 });
 
 const PDFContainer = styled.div`
@@ -125,6 +162,36 @@ function Billing() {
                             <View style={styles.address}>
                                 <Text style={styles.addressCity}>Rabat</Text>
                                 <Text style={styles.addressCityRight}>Rabat</Text>
+                            </View>
+                            <View style={{...styles.greyColor, ...styles.flex, marginTop: 40,}}>
+                                <Text>INVOICE ID</Text>
+                                <Text style={{ ...styles.right, ...styles.small}}>DUE DATE</Text>
+                                <Text></Text>
+                            </View>
+                            <View style={{...styles.blackColor, ...styles.flex,}}>
+                                <Text>Invoice #14020</Text>
+                                <Text></Text>
+                                <Text style={{ ...styles.right, ...styles.large}}>June 10, 2020</Text>
+                            </View>
+                            <View style={{...styles.flex, ...styles.lightGreyBackground, ...styles.padding, ...styles.greyColor, marginTop: 40}}>
+                                <Text>DESCRIPTION</Text>
+                                <Text>VOLUME</Text>
+                                <Text>COST</Text>
+                            </View>
+                            <View style={{...styles.flex, ...styles.padding, ...styles.blackColor, ...styles.borderBottom}}>
+                                <Text style={styles.large}>Electricity</Text>
+                                <Text>465 KWh</Text>
+                                <Text>DHS 745.00</Text>
+                            </View>
+                            <View style={{...styles.flex, ...styles.padding, ...styles.blackColor, ...styles.borderBottom}}>
+                                <Text style={styles.large}>Water</Text>
+                                <Text>33 m3</Text>
+                                <Text>DHS 140.00</Text>
+                            </View>
+                            <View style={{...styles.flex, ...styles.padding, ...styles.blackColor}}>
+                                <Text>Total amount</Text>
+                                <Text></Text>
+                                <Text>DHS 885</Text>
                             </View>
                         </Page>
                     </Document>
