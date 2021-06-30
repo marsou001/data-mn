f45ef5cd4935fad88b67355474ce2f89978023c0
Marouane Souda <marsou431@gmail.com>
2021-06-30 11:29:54 +0100

    wip: creating invoice PDF

--- src/components/Main/Billing/Billing.tsx
+++ src/components/Main/Billing/Billing.tsx
@@ -17,38 +17,119 @@ const Container = styled.div`
 
 const styles = StyleSheet.create({
     page: {
-        flexDirection: "row",
         backgroundColor: "#FFF",
+        padding: 20,
     },
     title: {
         textAlign: "center",
     },
     header: {
-        fontWeight: "bold",
+        fontSize: "30px",
+        fontWeight: "extrabold",
     },
-    section: {
-        margin: 10,
-        padding: 10,
-        flexGrow: 1,
+    subheader: {
+        color: '#666',
+        fontSize: '25px',
+        fontWeight: 'normal',
+    },
+    invoiced: {
+        color: '#666',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 40,
+    },
+    invoicedName: {
+        color: '#666',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 5,
+        marginBottom: 15,
+    },
+    invoicedLeft: {
+        textAlign: 'left',
+        width: '80px',
+    },
+    invoicedRight: {
+        textAlign: 'right',
+        width: 80,
+    },
+    recipient: {
+        color: '#000',
+        fontSize: '20px',
+        width: 110,
+    },
+    address: {
+        color: '#666',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    addressRues: {
+        width: 180,
+    },
+    addressArea: {
+        width: 120,
+    },
+    addressCity: {
+        width: 80,
+    },
+    addressRuesRight: {
+        textAlign: 'right',
+        width: 180,
+    },
+    addressAreaRight: {
+        textAlign: 'right',
+        width: 120,
+    },
+    addressCityRight: {
+        textAlign: 'right',
+        width: 80,
     },
 });
+
+const PDFContainer = styled.div`
+    width: 500px;
+    height: 500px;
+`;
 
 function Billing() {
     return (
         <Container>
             <DeviceID isOnBilling={true} />
-            <PDFViewer>
-                <Document>
-                    <Page size="A4" style={styles.page}>
-                        <View style={styles.title}>
-                            <Text style={styles.header}>Section #1</Text>
-                        </View>
-                        <View style={styles.section}>
-                            <Text>Section #2</Text>
-                        </View>
-                    </Page>
-                </Document>
-            </PDFViewer>
+            <PDFContainer>
+                <PDFViewer width='100%' height='100%'>
+                    <Document>
+                        <Page size="A4" style={styles.page}>
+                            <View style={styles.title}>
+                                <Text style={styles.header}>
+                                    Invoice from REDAL
+                                </Text>
+                                <Text style={styles.subheader}>Invoice #14020</Text>
+                            </View>
+                            <View style={styles.invoiced}>
+                                <Text style={styles.invoicedLeft}>Invoiced from</Text>
+                                <Text style={styles.invoicedRight}>Invoiced to</Text>
+                                <Text></Text>
+                            </View>
+                            <View style={styles.invoicedName}>
+                                <Text style={styles.recipient}>REDAL</Text>
+                                <Text style={styles.recipient}>Sami Lantry</Text>
+                            </View>
+                            <View style={styles.address}>
+                                <Text style={styles.addressRues}>6 Rue Al Hoceima,</Text>
+                                <Text style={styles.addressRuesRight}>15 Rue des Copilotes</Text>
+                            </View>
+                            <View style={styles.address}>
+                                <Text style={styles.addressArea}>Centre Ville</Text>
+                                <Text style={styles.addressAreaRight}>Agdal</Text>
+                            </View>
+                            <View style={styles.address}>
+                                <Text style={styles.addressCity}>Rabat</Text>
+                                <Text style={styles.addressCityRight}>Rabat</Text>
+                            </View>
+                        </Page>
+                    </Document>
+                </PDFViewer>
+            </PDFContainer>
         </Container>
     );
 }
